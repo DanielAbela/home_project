aa7611a0e9c411a4c2d87aaa41ad01df71269115
Daniel Abela <DanielAbela96@gmail.com>
2020-10-28 21:04:08 +0000

    Added more tests

--- home_project/tests/test_main.py
+++ home_project/tests/test_main.py
@@ -1,9 +1,16 @@
-from unittest import TestCase, mock
-from unittest.mock import Mock, patch, mock_open
+from unittest import TestCase
 
-from utils import check_if_float, round_data, read_file
+from main import validate_data, clean_up_data
+import pandas as pd
 
 
 class TestMain(TestCase):
-    def test_validate_data(self):
-        pass
+    def test_validate_data_return_true(self):
+        test_row = dict(Location="30, Monkhams Lane, Woodford Green, IG8 0NS", postcode="IG8 0NS")
+        self.assertTrue(validate_data(test_row))
+
+    def test_clean_up_data(self):
+        test_df = pd.DataFrame(columns=['lat', 'long'], data=[[5.6987456, 4.2456895]])
+        expected_df = pd.DataFrame(columns=['Latitude', 'Longitude'], data=[[5.699, 4.246]])
+        actual_df = clean_up_data(test_df)
+        pd.testing.assert_frame_equal(expected_df, actual_df)
